175a6edd7a9c68559ce7d51594740799e9b7480d
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-11-03 20:21:30 -0300

    feat: create content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+
+import { MovieCard } from "./MovieCard";
+import { useGenres } from "../contexts/GenreContext";
+import { api } from "../services/api";
+
+import '../styles/content.scss';
+
+interface Rating {
+  Source: string;
+  Value: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<Rating>;
+  Runtime: string;
+}
+
 export function Content() {
-  // Complete aqui
+  const { selectedGenre } = useGenres();
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    if (!selectedGenre) return;
+
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenre?.id}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenre]);
+
+  return (
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  )
 }
