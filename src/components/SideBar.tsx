5dd557158fcbbbb9deca8ae91fd5fdfe4faf0e93
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-11-03 20:21:57 -0300

    feat: create sidebar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
+import { useGenres } from "../contexts/GenreContext";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleSetSelectedGenre, selectedGenre } = useGenres();
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSetSelectedGenre(genre.id)}
+            selected={selectedGenre?.id === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
